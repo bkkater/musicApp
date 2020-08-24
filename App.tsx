1cb4ef6db64d9a8317cf6cfaca00820f02c9a0d1
bkkater <biancamedeios.bs10@gmail.com>
2020-08-24 15:01:22 -0300

    [add] new font

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { AppLoading } from 'expo';
 
 import { Ubuntu_400Regular, Ubuntu_500Medium, Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+import { Montserrat_300Light, Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold } from '@expo-google-fonts/montserrat'
+
 
 import AppStack from './src/routes/AppStack';
 
@@ -11,7 +13,12 @@ export default function App() {
   let [fontsloaded] = useFonts({
     Ubuntu_400Regular,
     Ubuntu_500Medium,
-    Ubuntu_700Bold
+    Ubuntu_700Bold,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold
   })
   if (!fontsloaded) {
     return <AppLoading />
